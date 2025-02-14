7ad0068e451d70d3eb652254ff13cd250befa911
Nikita Baboshin <baboshinikki90@gmail.com>
2025-02-14 20:17:56 +0400

    first commit

---
+++ src/main/java/ru/avoidedconnections/AvoidedconnectionsApplication.java
@@ -0,0 +1,13 @@
+package ru.avoidedconnections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoidedconnectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoidedconnectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/avoidedconnections/AvoidedconnectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avoidedconnections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvoidedconnectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
