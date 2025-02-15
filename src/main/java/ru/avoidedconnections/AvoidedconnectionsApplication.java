e6dd96c76140a02cedabe9c1c58822ae3ae409c1
Nikita Baboshin <baboshinikki90@gmail.com>
2025-02-15 22:44:23 +0400

    start

--- src/main/java/ru/avoidedconnections/AvoidedconnectionsApplication.java
+++ src/main/java/ru/avoidedconnections/AvoidedconnectionsApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvoidedconnectionsApplication {
-
+public class AvoidedConnectionsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AvoidedconnectionsApplication.class, args);
+		SpringApplication.run(AvoidedConnectionsApplication.class, args);
 	}
 
 }
