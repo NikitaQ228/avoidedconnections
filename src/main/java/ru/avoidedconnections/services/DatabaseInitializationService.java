27b7c8e8d82731a2e2ba085cc06d4fc599bd1fca
Dead-Ded <musailov2004@mail.ru>
2025-03-14 18:46:12 +0400

    Фикс

--- src/main/java/ru/avoidedconnections/services/DatabaseInitializationService.java
+++ src/main/java/ru/avoidedconnections/services/DatabaseInitializationService.java
@@ -2,13 +2,19 @@ package ru.avoidedconnections.services;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.ResourceUtils;
 import ru.avoidedconnections.model.User;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 @Service
@@ -17,9 +23,11 @@ public class DatabaseInitializationService {
     private JdbcTemplate jdbcTemplate;
     @Autowired
     private UserService userService;
+    @Autowired
+    ResourceLoader resourceLoader;
 
     @PostConstruct
-    public void initDatabase() {
+    public void initDatabase() throws IOException {
         if (!userService.addUser(new User("user1", "pass1")))
             return;
         userService.addUser(new User("user2", "pass2"));
@@ -27,13 +35,26 @@ public class DatabaseInitializationService {
         userService.addUser(new User("user4", "pass4"));
         userService.addUser(new User("user5", "pass5"));
         executeSqlScript("script.sql");
+
+
     }
 
-    private void executeSqlScript(String fileName) {
+    private void executeSqlScript(String fileName) throws IOException {
+
+//        try {
+////            File file = ResourceUtils.getFile("classpath:" + fileName);
+//            var file = resource.getFile();
+//            String sql = new String(Files.readAllBytes(file.toPath()));
+//            jdbcTemplate.execute(sql);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        String strSQL = null;
+        ClassPathResource classPathResource = new ClassPathResource("script.sql");
         try {
-            File file = ResourceUtils.getFile("classpath:" + fileName);
-            String sql = new String(Files.readAllBytes(file.toPath()));
-            jdbcTemplate.execute(sql);
+            byte[] binaryData = FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
+            strSQL = new String(binaryData, StandardCharsets.UTF_16);
+            jdbcTemplate.execute(strSQL);
         } catch (IOException e) {
             e.printStackTrace();
         }
