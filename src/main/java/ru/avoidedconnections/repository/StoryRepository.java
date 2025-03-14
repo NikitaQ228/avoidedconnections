db08b3d2c760b8b1e80a075889e1c4e3fd104fe8
Nikita Baboshin <baboshinikki90@gmail.com>
2025-03-14 17:59:02 +0400

    StoryRepository

--- src/main/java/ru/avoidedconnections/repository/StoryRepository.java
+++ src/main/java/ru/avoidedconnections/repository/StoryRepository.java
@@ -15,7 +15,12 @@ public interface StoryRepository extends JpaRepository<Story, Long> {
     // findById(Long id) реализован в интерфейсе CrudRepository
     @Query("SELECT s FROM Story s WHERE (:city IS NULL OR s.city LIKE :city) "
             + "AND (:query IS NULL OR (LOWER(s.head) LIKE LOWER(CONCAT('%', :query, '%')) "
-            + "OR LOWER(s.text) LIKE LOWER(CONCAT('%', :query, '%'))))")
+            + "OR LOWER(s.text) LIKE LOWER(CONCAT('%', :query, '%')))) ORDER BY s.date DESC")
     List<Story> searchStories(@Param("city") String city, @Param("query") String query);
 
+    @Query("SELECT s FROM Story s WHERE s.city = :city ORDER BY s.date DESC")
+    List<Story> findStoriesByCity(@Param("city") String city);
+
+    @Query("SELECT s FROM Story s ORDER BY s.date DESC")
+    List<Story> findAllStoriesSortedByDate();
 }
