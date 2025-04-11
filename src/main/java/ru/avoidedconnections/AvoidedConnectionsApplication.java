43349d8137bfcd41af6f5a7a15f4ad791585752f
Nikita Baboshin <baboshinikki90@gmail.com>
2025-04-12 00:19:10 +0400

    ...

--- src/main/java/ru/avoidedconnections/AvoidedConnectionsApplication.java
+++ src/main/java/ru/avoidedconnections/AvoidedConnectionsApplication.java
@@ -1,13 +1,9 @@
 package ru.avoidedconnections;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.avoidedconnections.model.Comment;
-import ru.avoidedconnections.repository.CommentRepository;
 
 // http://localhost:8080/swagger-ui/index.html
 @SpringBootApplication
